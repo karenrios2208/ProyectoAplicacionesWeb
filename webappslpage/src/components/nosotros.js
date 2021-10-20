3d025371a100cf5bec0d2fa8100c68bf5a70b8d2
MarioJim <mario.emilio.j@gmail.com>
2021-10-20 13:58:58 -0500

    Run formatter and delete other frontend folder

--- webappslpage/src/components/nosotros.js
+++ webappslpage/src/components/nosotros.js
@@ -4,13 +4,17 @@ export const Nosotros = (props) => {
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-md-6">
-            {" "}
-            <img src="img/birb2.jpg" className="img-responsive" alt="" />{" "}
+            {' '}
+            <img src="img/birb2.jpg" className="img-responsive" alt="" />{' '}
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
               <h2>Nosotros</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sed commodo nibh ante facilisis bibendum.</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                sed dapibus leo nec ornare diam sed commodo nibh ante facilisis
+                bibendum.
+              </p>
               <h3>¿Por qué nosotros?</h3>
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
@@ -25,14 +29,14 @@ export const Nosotros = (props) => {
                 </div>
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
-                  <ul>
-                    <li>Por esto</li>
-                    <li>Esto</li>
-                    <li>Esto</li>
-                    <li>Lo otro</li>
-                    <li>También esto</li>
-                    <li>Y esto</li>
-                  </ul>
+                    <ul>
+                      <li>Por esto</li>
+                      <li>Esto</li>
+                      <li>Esto</li>
+                      <li>Lo otro</li>
+                      <li>También esto</li>
+                      <li>Y esto</li>
+                    </ul>
                   </ul>
                 </div>
               </div>
--- webappslpage/src/pages/Home.js
+++ webappslpage/src/pages/Home.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Navigation } from '../components/navigation';
+import { Header } from '../components/header';
+import { Servicios } from '../components/servicios';
+import { Nosotros } from '../components/nosotros';
+import { Planes } from '../components/planes';
+import { Testimonios } from '../components/testimonios';
+import { Contacto } from '../components/contacto';
+import JsonData from '../data/data.json';
+import SmoothScroll from 'smooth-scroll';
+//import Products from './pages/Products';
+//import SignUp from './pages/SignUp';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import './Home.css';
 
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
 
-function Home() {
+const Home = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
   return (
-    <>
-    </>
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      <Servicios data={landingPageData.Servicios} />
+      <Nosotros data={landingPageData.Nosotros} />
+      <Planes data={landingPageData.Planes} />
+      <Testimonios data={landingPageData.Testimonios} />
+      <Contacto data={landingPageData.Contacto} />
+    </div>
   );
-}
+};
 
 export default Home;
