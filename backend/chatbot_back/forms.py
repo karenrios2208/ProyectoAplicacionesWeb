6b34843c761aff669d3dad96d3d0d8c1f21c366a
MarioJim <mario.emilio.j@gmail.com>
2021-11-03 18:50:08 -0600

    Implement registering users

--- backend/chatbot_back/forms.py
+++ backend/chatbot_back/forms.py
@@ -1,18 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+import wtforms_json
 from chatbot_back.models import Cuenta, Cliente
 
+wtforms_json.init()
+
+
 class Registration(FlaskForm):
+    class Meta:
+        csrf = False
+
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     nombres = StringField('Nombre(s)', validators=[DataRequired()])
     apellidos = StringField('Apellido(s)', validators=[DataRequired()])
     fecha_nacimiento = DateField('Fecha de Nacimiento')
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     DataRequired(), EqualTo('password')])
 
-    submit = SubmitField('Submit')
 
 class Login(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
